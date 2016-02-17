99f229867e14ad7b30ec9a5b197ac93a5ce24e0c
Vladimir Shmalko <vladimir.shmalko@gmail.com>
2016-02-17 15:11:43 -0800

    Working beta

--- src/box_api.erl
+++ src/box_api.erl
@@ -10,7 +10,7 @@
 
 %% Upload file
 upload(LFname,RFname,Client) ->
-	upload(LFname,RFname,"0",Client).
+	upload(LFname,RFname,<<"0">>,Client).
 
 upload(LFname,RFname,Parent,Client) when is_list(LFname) ->
 	case file:read_file(LFname) of
@@ -18,11 +18,11 @@ upload(LFname,RFname,Parent,Client) when is_list(LFname) ->
 		{error, Reason}	-> {error,0,Reason}
 	end;	
 
-upload(Body,RFname,Parent,Client) when is_binary(Body) ->
-	Uri = "https://upload.box.com/api/2.0/files/content",
+upload(Body,RFname,Parent,Client) when is_binary(Body)
+									  ,is_binary(RFname)
+									  ,is_binary(Parent) ->
+	Uri = <<"https://upload.box.com/api/2.0/files/content">>,
 	Boundary = "delimiter",
-	Params =  [{"uploadType","multipart"}],
-	Url = restc:construct_url(Uri,Params),
 	MetaData =  [
 					{<<"name">>,RFname}
 					,{<<"parent">>,[{<<"id">>,Parent}]}	
@@ -32,10 +32,11 @@ upload(Body,RFname,Parent,Client) when is_binary(Body) ->
 			{"application/json; charset=\"utf-8\"",jsx:encode(MetaData)}
 			,{"application/octet-stream",RFname,Body,data}
 			],Boundary)),
+	io:format("MultipartBody:~n~p~n",[MultipartBody]),
 	Headers = [{"Content-Length",integer_to_list(size(MultipartBody))}],
 	Type = "multipart/form-data; boundary=\"" ++ Boundary ++ "\"",
-	case oauth2c:request(post, Type, list_to_binary(Url), [200], Headers, MultipartBody, Client) of
-		{{ok,200,RHeaders,Replay},Client2} -> 
+	case oauth2c:request(post, Type, Uri, [201], Headers, MultipartBody, Client) of
+		{{ok,201,RHeaders,Replay},Client2} -> 
 			% io:format("Headers: ~p~n",[RHeaders]),
 			{ok,Replay};
 		{{error,Err,_,Replay},_} -> {error,Err,Replay};
@@ -49,16 +50,17 @@ upload(Body,RFname,Parent,Client) when is_binary(Body) ->
 make_dir(Folder,Parent,Client) when is_list(Folder) ->
 	make_dir(list_to_binary(Folder),Parent,Client);	
 
-make_dir(Folder,Parent,Client) when is_binary(Folder)->
-	Uri = <<"https://www.googleapis.com/drive/v2/files">>,
+make_dir(Folder,Parent,Client) when is_binary(Folder),
+									is_binary(Parent) ->
+	Uri = <<"https://api.box.com/2.0/folders">>,
 	%% Pass parameters as request's body for POST
 	Params =  [
-				{<<"mimeType">>,<<"application/vnd.google-apps.folder">>}
-				,{<<"title">>,Folder}
-				,{<<"parents">>,[[{<<"id">>,Parent}]]}
+				% {<<"mimeType">>,<<"application/vnd.google-apps.folder">>}
+				{<<"name">>,Folder}
+				,{<<"parent">>,[{<<"id">>,Parent}]}
 				],
-	case oauth2c:request(post, json, Uri, [200],[],Params,Client) of
-		{{ok,200,RHeaders,Replay},Client2} -> {ok,Replay};
+	case oauth2c:request(post, json, Uri, [201],[],Params,Client) of
+		{{ok,201,RHeaders,Replay},Client2} -> {ok,Replay};
 		{{error,Err,_,Replay},_} -> {error,Err,Replay};
 		{error,Err,Replay} -> {error,Err,Replay};
 		Req -> Req
@@ -66,40 +68,40 @@ make_dir(Folder,Parent,Client) when is_binary(Folder)->
 
 % Account info
 
-account_info(Client) ->
-	Uri = <<"https://www.googleapis.com/drive/v2/about">>,
-	case oauth2c:request(get, Uri, [200], Client) of
-		{{ok,200,RHeaders,Replay},Client2} -> {ok,Replay};
-		{{error,Err,_,Replay},_} -> {error,Err,Replay}
-	end.
+% account_info(Client) ->
+% 	Uri = <<"https://www.googleapis.com/drive/v2/about">>,
+% 	case oauth2c:request(get, Uri, [200], Client) of
+% 		{{ok,200,RHeaders,Replay},Client2} -> {ok,Replay};
+% 		{{error,Err,_,Replay},_} -> {error,Err,Replay}
+% 	end.
 
-% Get list
+% % Get list
 
-list(Query,Client) when is_list(Query)->
-	Uri = "https://www.googleapis.com/drive/v2/files",
-	Params =  [{"q",Query},{"spaces","drive"}],
-	Url = restc:construct_url(Uri,Params),
-	case oauth2c:request(get,list_to_binary(Url), [200], Client) of
-		{{ok,200,RHeaders,Replay},Client2} -> {ok,Replay};
-		{{error,Err,_,Replay},_} -> {error,Err,Replay};
-		{error,Err,Replay} -> {error,Err,Replay};
-		Req -> Req
-	end.
+% list(Query,Client) when is_list(Query)->
+% 	Uri = "https://www.googleapis.com/drive/v2/files",
+% 	Params =  [{"q",Query},{"spaces","drive"}],
+% 	Url = restc:construct_url(Uri,Params),
+% 	case oauth2c:request(get,list_to_binary(Url), [200], Client) of
+% 		{{ok,200,RHeaders,Replay},Client2} -> {ok,Replay};
+% 		{{error,Err,_,Replay},_} -> {error,Err,Replay};
+% 		{error,Err,Replay} -> {error,Err,Replay};
+% 		Req -> Req
+% 	end.
 
 
-refresh_token(ClientId,ClientSecret,RefreshToken,Client) ->
-	Uri = <<"https://www.googleapis.com/oauth2/v3/token">>,
-	Params = [
-			{"client_id",ClientId}
-			,{"client_secret",ClientSecret}
-			,{"grant_type","refresh_token"}
-			,{"refresh_token",RefreshToken}
-			],
-	case oauth2c:request(post,percent,Uri,[200],[],Params,Client) of
-		{{ok,200,RHeaders,Replay},Client2} -> {ok,Replay};
-		{{error,Err,_,Replay},_} -> {error,Err,Replay};
-		{error,Err,Replay} -> {error,Err,Replay}
-	end.
+% refresh_token(ClientId,ClientSecret,RefreshToken,Client) ->
+% 	Uri = <<"https://www.googleapis.com/oauth2/v3/token">>,
+% 	Params = [
+% 			{"client_id",ClientId}
+% 			,{"client_secret",ClientSecret}
+% 			,{"grant_type","refresh_token"}
+% 			,{"refresh_token",RefreshToken}
+% 			],
+% 	case oauth2c:request(post,percent,Uri,[200],[],Params,Client) of
+% 		{{ok,200,RHeaders,Replay},Client2} -> {ok,Replay};
+% 		{{error,Err,_,Replay},_} -> {error,Err,Replay};
+% 		{error,Err,Replay} -> {error,Err,Replay}
+% 	end.
 
 
 %%====================================================================
@@ -112,8 +114,8 @@ multipart_body([], Boundary) ->
 multipart_body([{Type,Body} | BodyList], Boundary) ->
     ["--", Boundary, "\r\n",
     "Content-Disposition: form-data; name=\"attributes\"","\r\n",
-     "Content-Type: ", Type, "\r\n",
-     "MIME-Version: 1.0","\r\n",
+     % "Content-Type: ", Type, "\r\n",
+     % "MIME-Version: 1.0","\r\n",
      "\r\n",
      Body,"\r\n"
      | multipart_body(BodyList, Boundary)];
